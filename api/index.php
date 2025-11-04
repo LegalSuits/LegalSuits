5b57cfff44d42966d3ec5ce1f7304cb348a55873
LegalSuits <lukas@sambes.sk>
2025-11-04 15:52:50 +0100

    Add simple index.php to serve as default index

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+// api/index.php - jednoduchá úvodná stránka / odkaz na testy
+?><!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>LegalSuits API</title>
+</head>
+<body>
+  <h1>LegalSuits</h1>
+  <p>Server beží. Testovacie súbory:</p>
+  <ul>
+    <li><a href="upload-test.html">upload-test.html</a></li>
+    <li><a href="process.php">process.php</a></li>
+  </ul>
+</body>
+</html>
